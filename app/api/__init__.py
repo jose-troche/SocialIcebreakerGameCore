f571fbddd772e37f82c7c8c4f80b9b783ca1d832
Jose Troche <jose.troche.coder@gmail.com>
2022-10-19 16:38:34 -0400

    Added more operations an in-memory persistence

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,15 +1,44 @@
 from typing import Union
+from pydantic import BaseModel
+import fastapi
+import logging
 
-from fastapi import FastAPI
 
-app = FastAPI()
+logger = logging.getLogger(__name__)
+
+app = fastapi.FastAPI()
+
+db = {}
+
+
+class Item(BaseModel):
+    id: int
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    logger.info("Got root")
+    return {"status": "alive"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items")
+def read_items():
+    return {'items': [db[id] for id in db]}
+
+
+@app.get("/items/{item_id}", status_code = 200)
+def read_item(item_id: int):
+    if item_id in db:
+        return db[item_id]
+    else:
+        raise fastapi.HTTPException(status_code = 404)
+
+
+@app.post("/items", status_code=201)
+def add_item(item: Item):
+    logger.info(f"Adding:\n{item}")
+    db[item.id] = item
+    
